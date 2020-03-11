2d51a74eb935da90521e5055d9b2d4a2b21b9d06
zhangheng <z1678760491@163.com>
2020-03-11 13:33:40 +0800

    '排序'

--- DS2-recursion/recursionPractise.py
+++ DS2-recursion/recursionPractise.py
@@ -1,31 +1,44 @@
 # 可视化递归算法
 # 递归绘制螺旋
-import turtle
+# import turtle
 
-myTurtle = turtle.Turtle()    # 乌龟
-myScreen = turtle.Screen()    # 绘制窗口
+# # myTurtle = turtle.Turtle()  # 🐢对象
+# # myScreen = turtle.Screen()  #  绘制窗口
 
-def tree(branchLen,t):
-    if branchLen > 5:
-        t.forward(branchLen)
-        t.right(45)
-        tree(branchLen-15,t)
-        t.left(90)
-        tree(branchLen-15,t)
-        t.right(45)
-        t.backward(branchLen)
-def main():
-    t = turtle.Turtle()
-    myScreen = turtle.Screen()
+# # def drawSpiral(myTurtle,lineLen):
+# #     if lineLen > 0:
+# #         myTurtle.forward(lineLen)
+# #         myTurtle.right(90)
+# #         drawSpiral(myTurtle,lineLen - 5)
 
-    t.left(90)
-    t.up()
-    t.backward(100)
-    t.down()
-    t.color("red")
-    tree(75,t)
-    myScreen.exitonclick()
-main()
+# # drawSpiral(myTurtle,100)
+# # myScreen.exitonclick() 
+
+
+# def tree(branchLen,t):
+#     if branchLen > 5:
+#         t.forward(branchLen)
+#         t.right(45)
+#         tree(branchLen-15,t)
+#         t.left(90)
+#         tree(branchLen-15,t)
+#         t.right(45)
+#         t.backward(branchLen)
+
+# def main():
+#     t = turtle.Turtle()
+#     myScreen = turtle.Screen()
+
+#     t.left(90)
+#     t.up()
+#     t.backward(100)
+#     t.down()
+#     t.color("red")
+#     tree(75,t)
+#     myScreen.exitonclick()
+
+# main()
+
 
 from pythonds.basic.stack import Stack
 
